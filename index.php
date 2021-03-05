bcbdb62e6dcf15d711ed8a69c5b3e403f0ca7f94
SSAXE01 <80072884+SSAXE01@users.noreply.github.com>
2021-03-04 19:25:44 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+include_once('dashborad_ui.html')
+include_once('donate_ui.html')
+include_once('learn_ui.html')
+include_once('login.html')
+include_once('news_ui.html')
+include_once('profile_ui.html')
+include_once('signup.html')
+include_once('starting_page.html')
+include_once('trashmap_ui.html')
+include_once('trashreport_ui.html')
+  
+?>
