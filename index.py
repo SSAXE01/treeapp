7e75807c827256e9284aa884b1982a2966fcfcb4
SSAXE01 <80072884+SSAXE01@users.noreply.github.com>
2021-03-13 20:52:05 -0500

    Var test

--- index.py
+++ index.py
@@ -8,4 +8,12 @@ si_user_pass = str(form.getvalue("si_user_pass"))
 si_mail = str(form.getvalue("si_mail"))
 fle=form['filename']
 
-print(si_user)
+fn=os.path.basename(fle.filename)
+open("starting_page.html"+fn, "wb").write(fle.file.read())
+
+print('<html>')
+print('<body><center>')
+print('<h1>test\n(%s)</h1>'%si_user)
+print('<img src=tem/%s></h1>'%fn)
+print('<h2>%s</h2>'%si_user_pass)
+print('</center></body></html>')
