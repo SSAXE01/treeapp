68d3773e1940f72a08171a3f09fb5733efe61c90
SSAXE01 <80072884+SSAXE01@users.noreply.github.com>
2021-03-13 20:38:29 -0500

    Open command test

--- index.py
+++ index.py
@@ -1,11 +1,1 @@
-import cgi,os
-
-print('content-type:text/html\r\n\r\n')
-
-form = cgi.FieldStorage()
-si_user = str(form.getvalue("si_user"))
-si_user_pass = str(form.getvalue("si_user_pass"))
-si_mail = str(form.getvalue("si_mail"))
-fle=form['filename']
-
 open("C:/Users/ranja/OneDrive/Desktop/MyApp/starting_page.html")
