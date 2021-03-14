c951984419574fcca554b36cbb9b8fd1429373be
SSAXE01 <80072884+SSAXE01@users.noreply.github.com>
2021-03-13 20:46:37 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,11 @@
-open("dashboard_ui.html")
+import cgi,os
+
+print('content-type:text/html\r\n\r\n')
+
+form = cgi.FieldStorage()
+si_user = str(form.getvalue("si_user"))
+si_user_pass = str(form.getvalue("si_user_pass"))
+si_mail = str(form.getvalue("si_mail"))
+fle=form['filename']
+
+print(si_user)
