fc2490eb306fd44d1e7a8f5f754b8bcde52910b9
SSAXE01 <80072884+SSAXE01@users.noreply.github.com>
2021-03-13 21:02:51 -0500

    not able to understand functions

--- index.py
+++ index.py
@@ -8,12 +8,10 @@ si_user_pass = str(form.getvalue("si_user_pass"))
 si_mail = str(form.getvalue("si_mail"))
 fle = form['starting_page.html']
 
-fn=os.path.basename(fle."starting_page.html")
 open("starting_page.html"+fn, "wb").write(fle.file.read())
 
 print('<html>')
 print('<body><center>')
 print('<h1>test\n(%s)</h1>'%si_user)
-print('<img src=tem/%s></h1>'%fn)
 print('<h2>%s</h2>'%si_user_pass)
 print('</center></body></html>')
