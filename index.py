67374f1b530b8bf94421ea7be251b69740a82052
SSAXE01 <80072884+SSAXE01@users.noreply.github.com>
2021-03-13 20:36:53 -0500

    CGI Test 2

--- index.py
+++ index.py
@@ -8,12 +8,4 @@ si_user_pass = str(form.getvalue("si_user_pass"))
 si_mail = str(form.getvalue("si_mail"))
 fle=form['filename']
 
-fn=os.path.basename(fle.filename)
-open("C:/Users/ranja/OneDrive/Desktop/MyApp/starting_page.html"+fn, "wb").write(fle.file.read())
-
-print('<html>')
-print('<body><center>')
-print('<h1>test\n(%s)</h1>'%si_user)
-print('<img src=tem/%s></h1>'%fn)
-print('<h2>%s</h2>'%si_user_pass)
-print('</center></body></html>')
+open("C:/Users/ranja/OneDrive/Desktop/MyApp/starting_page.html")
