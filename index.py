78dbba51c4aeaf0c4a516cc95ac0cad0ca04d24c
SSAXE01 <80072884+SSAXE01@users.noreply.github.com>
2021-03-16 16:40:42 -0400

    Test

--- index.py
+++ index.py
@@ -1,17 +1,5 @@
-import cgi,os
+import requests
 
-print('content-type:text/html\r\n\r\n')
+response = requests.get('https://randomfox.ca/floof')
 
-form = cgi.FieldStorage()
-si_user = str(form.getvalue("si_user"))
-si_user_pass = str(form.getvalue("si_user_pass"))
-si_mail = str(form.getvalue("si_mail"))
-fle = form['starting_page.html']
-
-open("starting_page.html"+fn, "wb").write(fle.file.read())
-
-print('<html>')
-print('<body><center>')
-print('<h1>test\n(%s)</h1>'%si_user)
-print('<h2>%s</h2>'%si_user_pass)
-print('</center></body></html>')
+print(response)
