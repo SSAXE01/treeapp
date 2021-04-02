b44a09bde9509304970dbef8fc0b479763a282f9
SSAXE01 <80072884+SSAXE01@users.noreply.github.com>
2021-04-02 12:15:53 -0400

    Requests test #1

--- index.py
+++ index.py
@@ -1,5 +1,8 @@
 import requests
 
-response = requests.get('https://randomfox.ca/floof')
-
-print(response)
+url = 'https://treeapp102.herokuapp.com'
+credinitals = {"si_user": "Saxena",
+               "si_user_pass": "Sharvil",
+               "si_mail": "sharvil@gmail.com"}
+r = requests.get(url, auth=credinitals)
+open('dashboard_ui.html')
